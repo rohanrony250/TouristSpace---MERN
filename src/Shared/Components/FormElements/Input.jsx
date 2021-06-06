b54dc1557c537b99e6c957573dd11ac04d0dcf8e
rohanrony250 <rohanrony@hotmail.com>
2021-06-06 16:41:25 +0300

    form-validators

--- src/Shared/Components/FormElements/Input.jsx
+++ src/Shared/Components/FormElements/Input.jsx
@@ -1,16 +1,58 @@
-import React from "react"
+import React, {useReducer}from "react"
 import "./Input.scss"
+import { validate } from "../../Util/validators"
+
+
+
+const InputReducer = (state, action) =>
+{
+    switch(action.type)
+    {
+        case 'CHANGE':
+            return{
+                ...state,
+                value: action.val,
+                isValid: validate(action.val, action.validators)
+            };
+        case 'TOUCH':
+            return{
+                ...state,
+                isTouched: true
+            }
+        default: 
+            return state;
+    
+    }
+}
+
 
 
 const Input = (props) =>
 {
-    const element = props.element === 'input' ? <input id = {props.id} type = {props.type} placeholder = {props.placeholder}/> : <textarea id = {props.id} rows = {props.rows || 3} />
+    const [inputState, dispatch] = useReducer(InputReducer, {value: '', isValid: false, isTouched: false})
+    const changeHandler = (event) =>
+    {
+        dispatch({type: 'CHANGE', val: event.target.value, validators: props.validators})
+    }
+    const touchHandler = () =>
+    {
+        dispatch({
+
+            type: 'TOUCH',
+
+
+        })
+    }
+    const element = (props.element === 'input') ? 
+        <input id = {props.id} type = {props.type} onBlur = {touchHandler} placeholder = {props.placeholder} onChange = {changeHandler} value = {inputState.value}/> 
+        : <textarea onChange = {changeHandler} id = {props.id} rows = {props.rows || 3} value = {inputState.value} onBlur={touchHandler}/>
     return(
-        <div className = {`form-control`}> 
+        <div className = {`form-control ${!inputState.isValid && inputState.isTouched && 'form-control--invalid'}`}> 
             <label htmlFor={props.id}>
                 {props.label}    
             </label>
             {element}
+            {!inputState.isValid && inputState.isTouched && <p>{props.errorText}</p>}
         </div>
     )
 }
--- src/pages/Places/NewPlaces-Page.jsx
+++ src/pages/Places/NewPlaces-Page.jsx
@@ -1,14 +1,21 @@
 import React from "react"
 import "./NewPlaces.scss"
 import Input from "../../Shared/Components/FormElements/Input"
-
-
+import { VALIDATOR_REQUIRE } from "../../Shared/Util/validators"
+ 
 
 const NewPlace = () =>
 {
     return (
         <form className = "place-form">
-            <Input element = "input" label = "Title"/>
+            <Input 
+                element = "input" 
+                label = "Title" 
+                validators = {[VALIDATOR_REQUIRE()]} 
+                errorText = "Please enter a valid title"
+            
+            
+            />
         </form>
     )
 }
