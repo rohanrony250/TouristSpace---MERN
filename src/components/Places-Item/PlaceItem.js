5b5a076d0533ff80ddf588e1b1b5c2873c10c992
rohanrony250 <rohanrony@hotmail.com>
2021-06-16 11:13:26 +0300

    delete-place-functionality

--- src/components/Places-Item/PlaceItem.js
+++ src/components/Places-Item/PlaceItem.js
@@ -7,8 +7,22 @@ import Map from "../../Shared/Components/UI-Elements/Map/Map"
 const PlaceItem = props =>
 { 
     const [showMap, setShowMap] = useState(false)
+    const [showModal, setshowModal] = useState(false)
     const openMap = () => setShowMap(true)
     const closeMap = () => setShowMap(false)
+    const showDeleteWarningHandler = () =>
+    {
+        setshowModal(true)
+    }
+    const cancelDeleteHandler = () =>
+    {
+        setshowModal(false)
+    }
+    const confirmDeleteHandler = () =>
+    {
+        setshowModal(false)
+        console.log('DELETING....')
+    }
     return(
         <React.Fragment>
             <Modal 
@@ -22,6 +36,16 @@ const PlaceItem = props =>
                 <div className = "map-container">
                     <Map coordinates = {props.coordinates}/>
                 </div>
+            </Modal>
+            <Modal show = {showModal}  header = "Are you sure ?" footerClass = "place-item__modal-actions" footer = {
+                <>
+                    <Button inverse onClick = {cancelDeleteHandler}>CANCEL</Button>
+                    <Button danger onClick = {confirmDeleteHandler}>DELETE</Button>
+                </>
+            }>
+                <p>
+                    Do you want to proceed and delete this place? Please remember it can't be undone thereafter.
+                </p>
             </Modal>
             <li className = "place-item">
                 <Card className = "place-item__content">
@@ -48,7 +72,7 @@ const PlaceItem = props =>
                         <Button to={`/places/${props.id}`}>
                             EDIT
                         </Button>
-                        <Button danger>
+                        <Button danger onClick ={showDeleteWarningHandler}>
                             DELETE
                         </Button>
                     </div>
